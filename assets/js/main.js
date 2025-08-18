fa68999c1dbcec8dfe61bbdeb4fec9b4390d6636
taminur <taminur@live.com>
2025-08-18 23:57:01 +0600

    other pages are added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
-// Auto-update year
-document.getElementById('year').textContent = new Date().getFullYear();
+function updateYear() {
+    document.getElementById('year').textContent = new Date().getFullYear();
+    }
+
+// Run after footer is loaded
+async function include(file, id, callback) {
+    const response = await fetch(file);
+    const content = await response.text();
+    document.getElementById(id).innerHTML = content;
+    if (callback) callback();
+    }
+
+include("/includes/header.html", "header");
+include("/includes/footer.html", "footer", updateYear);
