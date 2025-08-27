57a216a1b77e0748362e21457577fb8a7461950d
taminur <taminur@live.com>
2025-08-28 00:45:59 +0600

    background, font color updated, home page animation added

---
+++ assets/js/finisher-config.js
@@ -0,0 +1,39 @@
+new FinisherHeader({
+  "count": 13,
+  "size": {
+    "min": 2,
+    "max": 40,
+    "pulse": 0
+  },
+  "speed": {
+    "x": {
+      "min": 0,
+      "max": 0.8
+    },
+    "y": {
+      "min": 0,
+      "max": 0.2
+    }
+  },
+  "colors": {
+    "background": "#15182e",
+    "particles": [
+      "#ff926b",
+      "#87ddfe",
+      "#acaaff",
+      "#1bffc2",
+      "#f9a5fe"
+    ]
+  },
+  "blending": "screen",
+  "opacity": {
+    "center": 1,
+    "edge": 1
+  },
+  "skew": 0,
+  "shapes": [
+    "c",
+    "s",
+    "t"
+  ]
+});
